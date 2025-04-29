b517cc53f046d4616074a504ebbd312ca96e11af
AL W <allan.wolfe89@gmail.com>
2025-04-29 14:22:56 -0500

    moved build commands to netlify, simplified a few things

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,3 @@
-// vite.config.js
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
-const path = require('path');
-
-module.exports = defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    cssCodeSplit: false, // This ensures CSS is not split into chunks
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
-});
+module.exports = {
+  plugins: []
+}
